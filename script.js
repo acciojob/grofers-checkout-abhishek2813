01aa0d639625465164682dfdfa4b397ea78830af
Abhishek Pandey <98042094+abhishek2813@users.noreply.github.com>
2023-03-01 13:06:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,22 +3,16 @@ getSumBtn.append("Get Total Price");
 document.body.appendChild(getSumBtn);
 
 const getSum = () => {
-//Add your code here
-	var table = document.getElementById("myTable"),
-  sumVal = 0;
-for (var i = 1; i < table.rows.length; i++) {
-  sumVal = sumVal + parseFloat(table.rows[i].cells[1].innerHTML);
-}
-  let row = table.insertRow(-1); // We are adding at the end
-   
-      // Create table cells
-      let c1 = row.insertCell(0);
-      let c2 = row.insertCell(1);
-      
-   
-      // Add data to c1 and c2
-      c1.innerText = "total price"
-      c2.innerText = sumVal
+  const priceArr = document.querySelectorAll('.price')
+   let sum=0;
+	priceArr.forEach(price => {
+		sum+=parseFloat(price.innerText)
+	})
+const table = document.querySelector('table');
+const newRow = table.insertRow();
+const newCell = newRow.insertCell();
+newCell.colSpan = 2;
+newCell.innerText = `Total: ${sum.toFixed(2)}`;
 
 };
 
